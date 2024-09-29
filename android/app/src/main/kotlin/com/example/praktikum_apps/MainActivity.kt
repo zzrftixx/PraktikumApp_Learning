48000b3e2ee6acb1c63bbb6539ed23c9a254d0fe
zzrftixx <andragiri.opy@gmail.com>
2024-09-29 15:06:10 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/praktikum_apps/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widget/mytext_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({Key? key}) : super(key: key);
+  Widget build(BuildContext context) {
+    return const Text('Hello, Andra Aresfi');
+  }
+}
